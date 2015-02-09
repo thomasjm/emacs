b7575f58ef741c1398cb6828a99224c1449b679e
Tom McLaughlin <tomm@dropbox.com>
2015-02-09 14:03:19 -0800

    Check in some random packages

---
+++ .emacs.d/elpa/psci-20150114.923/psci-pkg.el
@@ -0,0 +1,11 @@
+(define-package "psci" "20150114.923" "Major mode for purescript repl psci"
+  '((purescript-mode "13.10")
+    (dash "2.9.0")
+    (s "1.9.0")
+    (f "0.17.1")
+    (deferred "0.3.2"))
+  :url "https://github.com/ardumont/emacs-psci" :keywords
+  '("purescript" "psci" "repl" "major" "mode"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ .emacs.d/elpa/shm-20150126.1325/shm-pkg.el
@@ -0,0 +1,5 @@
+(define-package "shm" "20150126.1325" "Structured Haskell Mode" 'nil :keywords
+  '("development" "haskell" "structured"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
