537957047bb69db1ea0605acf8336e15a878620b
Tom McLaughlin <tom@codedown.io>
2020-01-10 19:55:32 -0800

    Delete some packages

--- .emacs.d/elpa/color-theme-gruber-darker-1/color-theme-gruber-darker-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; color-theme-gruber-darker-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("color-theme-gruber-darker.el") (21702
-;;;;;;  39720 337030 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; color-theme-gruber-darker-autoloads.el ends here
--- .emacs.d/elpa/color-theme-twilight-0.1/color-theme-twilight-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; color-theme-twilight-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("color-theme-twilight.el") (21702 39708
-;;;;;;  97256 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; color-theme-twilight-autoloads.el ends here
